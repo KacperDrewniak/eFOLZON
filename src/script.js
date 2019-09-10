8d4aebef3857014b90ee8ef2d760c6db22f7170c
Kacper Drewniak <drewniakkacper1@gmail.com>
2019-09-10 15:06:34 +0200

    Responsive

--- src/script.js
+++ src/script.js
@@ -3,3 +3,9 @@ $("body").niceScroll({});
 
 // Activate AOS
 AOS.init();
+
+menuClick = document.querySelector('div.icon')
+menu = document.querySelector('div.menu')
+menuClick.addEventListener('click',()=>{
+    menu.classList.toggle('active')
+})
