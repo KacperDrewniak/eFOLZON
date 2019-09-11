7a78f39fea049dad9313667df0cc6612bd0555d3
Kacper Drewniak <drewniakkacper1@gmail.com>
2019-09-11 10:44:40 +0200

    Commit with Animations

--- src/script.js
+++ src/script.js
@@ -8,4 +8,47 @@ menuClick = document.querySelector('div.icon')
 menu = document.querySelector('div.menu')
 menuClick.addEventListener('click',()=>{
     menu.classList.toggle('active')
+    menuClick.classList.toggle('active')
+})
+
+blackMenu = document.querySelector('div.menu')
+blackMenu.addEventListener('click',()=>{
+    blackMenu.classList.toggle('active')
+})
+
+
+// Scroll To
+$('nav ul li:nth-child(2)').click(()=>{
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("main").offset().top
+    }, 500);
+})
+
+$('div.menu ul li:nth-child(2)').click(()=>{
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("main").offset().top
+    }, 500);
+})
+
+$('nav ul li:nth-child(3)').click(()=>{
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("aside").offset().top
+    }, 1500);
+})
+$('div.menu ul li:nth-child(3)').click(()=>{
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("aside").offset().top
+    }, 1500);
+})
+
+
+$('nav ul li:nth-child(4)').click(()=>{
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("article").offset().top
+    }, 2500);
+})
+$('div.menu ul li:nth-child(4)').click(()=>{
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("article").offset().top
+    }, 2500);
 })
